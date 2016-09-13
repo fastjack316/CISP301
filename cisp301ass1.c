2704d55eccc2d8bebd7fb6153ad9edee90e0f793
Seth Mackling <Seth.Mackling@gmail.com>
2016-09-12 19:48:51 -0700

    replace with modified version
    strcpy/strcat collection to arrange them and formatting fixes to output

--- cisp301ass1.c
+++ cisp301ass1.c
@@ -4,28 +4,29 @@
 	Author: Seth Mackling
 	Date: 8/31/2016
 	Purpose: Assemble fake employee pay statement
+	
+	9/12/2016 Seth Mackling strcpy strcat series added, formatting fixed for columns
 
 */
 
 #include <stdio.h>
 #include <string.h>
 
-    #define REPORTHEADINGS "     Name                   Pay Rate     Hours     Gross    Tax    Net\n" //formatting is broken right now
-  	#define format         "     %-15s%-2s%-20s%8.2f%10.2f%10.2f%8.2f%10.2f\n" //figure out the right numbers here and how to append , between first last
+    #define REPORTHEADINGS "     Name                   Pay Rate     Hours     Gross Pay     Tax Paid     Net Pay\n" //formatting is broken right now
+  	#define format         "     %-23s%7.2f%11.2f%14.2f%13.2f%12.2f\n" //figure out the right numbers here and how to append , between first last
 
 int main(void)
     {
 			char answer;
 			int employeecount;
 			int silly; // the need for this variable seems silly to me
-			char comma[1]; //incredibly inefficient way to get a comma inserted
+			char result[52+1]; //output
 			
-			strcpy(comma, ", ");
 			
               struct employees // store all employee data linked together
                      {
-                     char firstname[15];
-                     char lastname[20];
+                     char firstname[10+1];
+                     char lastname[20+1];
                      float payrate;
                      float hours;
 					 float gross;
@@ -34,8 +35,8 @@ int main(void)
                      } emp[9]; // inefficiently assign static memory because i don't know how to use pointers yet
 				
 			employeecount = 0;  //initialize a counter to progress through array
-			do
-		{
+		do
+			{
 	      printf("Enter employee's first name ");  // input section
 	      scanf("%s",emp[employeecount].firstname);
 		  printf("Enter employee's last name ");
@@ -47,7 +48,7 @@ int main(void)
 	      /* processing block 
 		     determines need for overtime, then applies tax and net, stores all data in array */
 	      if (emp[employeecount].hours <= 40)
-             emp[employeecount].gross = emp[employeecount].hours * emp[employeecount].payrate;
+            	emp[employeecount].gross = emp[employeecount].hours * emp[employeecount].payrate;
           else
              emp[employeecount].gross = 40 * emp[employeecount].payrate +1.5*emp[employeecount].payrate*(emp[employeecount].hours-40);
 		emp[employeecount].tax = emp[employeecount].gross * 0.15; // calculate tax using static value
@@ -60,10 +61,11 @@ int main(void)
 		printf(REPORTHEADINGS);
 		for (silly = 0; silly < employeecount; silly++) // iterate outputs through array
 		{
-				printf(format,emp[silly].lastname,comma,emp[silly].firstname,emp[silly].payrate,emp[silly].hours,emp[silly].gross,emp[silly].tax,emp[silly].net); 
+				strcpy(result,emp[silly].lastname); //constructs the single string name
+				strcat(strcat(result,", "),emp[silly].firstname);
+				printf(format,result,emp[silly].payrate,emp[silly].hours,emp[silly].gross,emp[silly].tax,emp[silly].net); 
 		}
 		fflush(stdin); // allows viewing output
 	    getchar();
 		return 0;
-	}
-	
+}
